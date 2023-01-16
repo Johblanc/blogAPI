7a3bb0b8773330ce342c02ec6bc18f6d8baab20d
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-01-16 21:30:25 +0100

    Feat : Ajout de la methode add à la class ArticlesServices

--- src/services/articlesServices.ts
+++ src/services/articlesServices.ts
@@ -27,4 +27,15 @@ export class ArticlesServices{
         return undefined
     }
 
+    async add(user_id : number, title : string , content : string) : Promise<TArticle | undefined>
+    {
+        const data : QueryResult<TArticle> = await client.query('INSERT INTO articles (user_id, title ,content) VALUES ($1,$2,$3) RETURNING *', [user_id,title,content]);
+
+        if(data.rowCount)
+        {
+            return data.rows[0];
+        }
+        return undefined
+    }
+
 }
