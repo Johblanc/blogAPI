8e5630feb068e76d44217044701a18b32b045713
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-01-16 23:46:26 +0100

    Feat : Ajout de la methode getAll à la class ArticlesController

--- src/controllers/articlesController.ts
+++ src/controllers/articlesController.ts
@@ -1,8 +1,29 @@
+import { Request, Response } from "express";
+import { Responser } from "../module/Responser";
+import { ArticlesServices } from "../services/articlesServices";
 
 
-
+const articlesServices = new ArticlesServices()
 
 export class ArticlesController 
 {
-
+    async getAll (req : Request , res : Response)
+    {
+        let responcer = new Responser(req, res) ;
+        
+        try 
+        {
+            const data = await articlesServices.getAll();
+    
+            responcer.status = 200 ;
+            responcer.message = `Récupération de ${data?.length || 0} articles` ;
+            responcer.data = data ;
+            responcer.send() ;
+        }
+        catch (err : any) 
+        {
+            console.log(err.stack);
+            responcer.send() ;
+        }
+    }
 }
