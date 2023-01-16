98baaa581cef48d1fb216b249da61dafafefb6f8
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-01-16 23:17:38 +0100

    Feat : Ajout de la methode getAll à la class AdminLvlServices

--- src/services/adminLvlServices.ts
+++ src/services/adminLvlServices.ts
@@ -3,5 +3,15 @@ import { client } from "../module/clientData";
 import { TAdminLvl } from "../types/TAdminLvl";
 
 export class AdminLvlServices{
+    async getAll() : Promise<TAdminLvl[] | undefined>
+    {
+        const data : QueryResult<TAdminLvl> = await client.query('SELECT * FROM admin_lvl');
+
+        if(data.rowCount)
+        {
+            return data.rows;
+        }
+        return undefined
+    }
 
 }
