fdb75d19f90c129c9e36ba9aac46045b08098fcb
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-01-16 11:14:11 +0100

    feat : Création du fichier index.ts

---
+++ src/index.ts
@@ -0,0 +1,73 @@
+// imports
+// npm i express
+// npm i body-parser
+
+import { Request, Response } from "express";
+
+const express = require('express');
+
+require('dotenv').config()
+
+const Responcer = require('./module/Responcer') ;
+
+
+// declarations
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+app.use(function (req : Request, res : Response, next: () => void) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+    // Pass to next layer of middleware
+    next();
+});
+
+//app.use('/api/tickets', ticketsRouter);
+//app.use('/api/users', usersRouter);
+
+
+app.all('*', async (req: Request, res: Response) => 
+{
+    let rpcr = new Responcer({
+        status : 404,
+        message : `Cette requête n'existe pas`
+    })
+    console.log(`* | ${rpcr.info()}`);
+    console.log(req);
+    res = rpcr.send(res)
+});
+
+// ecoute le port 8000
+app.listen(port, () => {
+    console.log(`Example app listening on port http://localhost:${port}`)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
