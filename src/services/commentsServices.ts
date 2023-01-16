dfc6ac59d2f15d94ce293523eebe4fd7e5793a44
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-01-16 22:34:04 +0100

    Feat : Ajout de la methode getByArticleId à la class CommentsServices

--- src/services/commentsServices.ts
+++ src/services/commentsServices.ts
@@ -1,4 +1,17 @@
+import { QueryResult } from "pg";
+import { client } from "../module/clientData";
+import { TComment } from "../types/TComment";
 
 export class CommentsServices{
+    async getByArticleId() : Promise<TComment[] | undefined>
+    {
+        const data : QueryResult<TComment> = await client.query('SELECT * FROM comments');
+
+        if(data.rowCount)
+        {
+            return data.rows;
+        }
+        return undefined
+    }
 
 }
