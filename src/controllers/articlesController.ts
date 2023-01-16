987791559f67c5da786fdc01da0e8f9cbec41fe4
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-01-17 00:00:17 +0100

    Feat : Ajout de la methode getByid à la class ArticlesController

--- src/controllers/articlesController.ts
+++ src/controllers/articlesController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { faillingId } from "../module/faillingTest";
 import { Responser } from "../module/Responser";
 import { ArticlesServices } from "../services/articlesServices";
 
@@ -7,7 +8,7 @@ const articlesServices = new ArticlesServices()
 
 export class ArticlesController 
 {
-    async getAll (req : Request , res : Response)
+    async getAll (req : Request , res : Response) : Promise<void>
     {
         let responcer = new Responser(req, res) ;
         
@@ -23,6 +24,46 @@ export class ArticlesController
         catch (err : any) 
         {
             console.log(err.stack);
+            responcer.send() ;
+        }
+    }
+
+    async getById (req : Request , res : Response) : Promise<void>
+    {
+        let responcer = new Responser(req, res) ;
+        const id = Number(req.params.id) ;
+        
+        // Vérifiction du Type de l'id entrante
+        if (faillingId(id))
+        {
+            responcer.status = 400 ;
+            responcer.message = `${id} n'est pas un nombre entier` ;
+            responcer.send() ;
+            return ;
+        } 
+    
+        try 
+        {
+            const data = await articlesServices.getById(id);
+    
+            // Vérifiction de l'existence de l'id
+            if (data) {
+                
+                responcer.status = 200 ;
+                responcer.message = `Récupération de l'article ${id}` ;
+                responcer.data = data ;
+                responcer.send() ;
+                return ;
+    
+            } 
+            responcer.status = 404 ;
+            responcer.message = `L'article ${id} n'existe pas` ;
+            responcer.send() ;
+            
+        }
+        catch (err : any)
+        {
+            console.log(err.stack)
             responcer.send() ;
         }
     }
