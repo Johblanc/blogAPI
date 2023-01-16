5b4ae3c90f43423d05f425e9b5e012264e431f93
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-01-16 21:28:53 +0100

    Feat : Ajout de la methode getAll à la class ArticlesServices

--- src/services/articlesServices.ts
+++ src/services/articlesServices.ts
@@ -5,5 +5,15 @@ import {client} from '../module/clientData';
 import { TArticle } from '../types/TArticle';
 
 export class ArticlesServices{
+    async getAll() : Promise<TArticle[] | undefined>
+    {
+        const data : QueryResult<TArticle> = await client.query('SELECT * FROM articles');
+
+        if(data.rowCount)
+        {
+            return data.rows;
+        }
+        return undefined
+    }
 
 }
