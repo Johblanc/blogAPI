02df27beb574246c7de114fe989fae20c0d8454c
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-01-16 18:58:45 +0100

    Fix : Mise à jour des imports

--- src/module/clientData.ts
+++ src/module/clientData.ts
@@ -1,14 +1,12 @@
 
-const { Client } = require('pg');
+import { Client } from 'pg';
 
-const client = new Client({
+export const client = new Client({
     user: process.env.DB_USERNAME,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME, 
     password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    port: Number(process.env.DB_PORT),
 });
 
 client.connect();
-
-module.exports = client
