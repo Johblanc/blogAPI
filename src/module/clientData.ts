d1d2c11dd0780ad1679de917cd90c6de0baee4d2
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-01-16 13:09:03 +0100

    feat : ajout du module clientData

---
+++ src/module/clientData.ts
@@ -0,0 +1,14 @@
+
+const { Client } = require('pg');
+
+const client = new Client({
+    user: process.env.DB_USERNAME,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME, 
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
+
+client.connect();
+
+module.exports = client
