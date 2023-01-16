5ed36bcb3dba247efdcf2383014fd24e4542a844
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-01-16 22:01:55 +0100

    Fix : Suppression de la propriété titre

--- src/types/TComment.ts
+++ src/types/TComment.ts
@@ -4,7 +4,6 @@ export type TComment = {
     id          : number ,
     user_id     : number ,
     article_id  : number ,
-    title       : string ,
     content     : string ,
     created     : Date ,
     modified    : Date
