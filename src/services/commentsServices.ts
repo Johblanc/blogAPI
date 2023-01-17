755b5909809994c4f78d2871cee011085af973c9
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-01-17 12:03:51 +0100

    Feat : Ajout de la methode getAll à la class CommentsServices

--- src/services/commentsServices.ts
+++ src/services/commentsServices.ts
@@ -3,9 +3,19 @@ import { client } from "../module/clientData";
 import { TComment } from "../types/TComment";
 
 export class CommentsServices{
-    async getByArticleId() : Promise<TComment[] | undefined>
+    async getAll() : Promise<TComment[] | undefined>
     {
         const data : QueryResult<TComment> = await client.query('SELECT * FROM comments');
+
+        if(data.rowCount)
+        {
+            return data.rows;
+        }
+        return undefined
+    }
+    async getByArticleId(article_id :number) : Promise<TComment[] | undefined>
+    {
+        const data : QueryResult<TComment> = await client.query('SELECT * FROM comments WHERE article_id = $1',[article_id]);
 
         if(data.rowCount)
         {
@@ -46,7 +56,7 @@ export class CommentsServices{
 
     async deleteByArticleId(articleId :number) : Promise<number>
     {
-        const data : QueryResult<TComment> = await client.query('DELETE FROM comments WHERE article_id = $1', [ articleId ] );
+        const data : QueryResult<TComment> = await client.query('DELETE FROM comments WHERE id = $1', [ articleId ] );
 
         return data.rowCount;
     }
