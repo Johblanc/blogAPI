838ed6b6af1eab3716d951a2cd4cc7b74f76745b
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-01-17 23:25:34 +0100

    Fix : Suppression des types inutiles

--- src/types/TToken.ts
+++ src/types/TToken.ts
@@ -1,16 +1,3 @@
-import { JwtPayload } from "jsonwebtoken";
-
-export interface RequestWithUserRole extends Request
-{
-    id?: string | JwtPayload | undefined,
-    adminLvl?: string | JwtPayload | undefined,
-    headers: HeaderWithAuthorization
-}
-
-interface HeaderWithAuthorization extends Headers
-{
-    authorization?: string
-}
 
 export type TToken = {
     id : number ,
